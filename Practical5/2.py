35be4f79bb5dbc9d6d865ed84d90ca2c9a66deb2
Deloste <13586663666@139.com>
2025-03-19 15:27:11 +0800

    Update 2.py

--- Practical5/2.py
+++ Practical5/2.py
@@ -1,11 +1,13 @@
 import matplotlib.pyplot as plt#import matplotlib
 uk_countries=[57.11,3.13,1.91,5.45]
+uklabels=['England','Wales','Northern Ireland','Scotland']#provide labels
 china_provinces=[65.77,41.88,45.28,61.27,85.15]
+cnlabels=['Zhejiang','Fujian','Jiangxi','Anhui','Jiangsu']#provide labels
 uk=sorted(uk_countries)#sort uk_countries
 cn=sorted(china_provinces)#sort china_provinces
 print(uk)#print sorted data
 print(cn)#print sorted data
-plt.pie(uk, labels=None, autopct=None, colors=None, startangle=0)
-plt.show()#shoe the pie of uk
-plt.pie(cn, labels=None, autopct=None, colors=None, startangle=0)
-plt.show()#shoe the pie of cn
+plt.pie(uk, labels=uklabels, autopct=None, colors=None, startangle=0)
+plt.show()#show the pie of uk
+plt.pie(cn, labels=cnlabels, autopct=None, colors=None, startangle=0)
+plt.show()#show the pie of cn
