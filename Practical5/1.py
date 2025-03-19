aa6e2e047f2a84f0515b80c4bd60dd2d279499d0
Deloste <13586663666@139.com>
2025-03-19 15:38:41 +0800

    Update 1.py

--- Practical5/1.py
+++ Practical5/1.py
@@ -1,8 +1,10 @@
 import matplotlib.pyplot as plt#import matplotlib
 dict={'JavaScript':62.3,'HTML':52.9,'Python':51,'SQL':51,'TypeScript':38.5}
 print(dict)#print the dictionary
-i=input('Language:')#get the inputted language
-print(dict[i])#output the matched value
 k=dict.keys()#keys of dict
 v=dict.values()#values of dict
-plt.bar(k,v,color='blue')#draw the bar
+bars = plt.bar(k, v, color='blue')#draw the bar
+for bar in bars:
+    height = bar.get_height() #get height
+    plt.text(bar.get_x() + bar.get_width() / 2, height, str(height), ha='center', va='bottom') #add data on the bars
+plt.show()#show the bar
