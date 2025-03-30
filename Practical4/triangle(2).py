3c4d0ff40217f430b0f9810dfb8ad7d7d0e59d14
Deloste <13586663666@139.com>
2025-03-30 23:42:14 +0800

    Update triangle(2).py

--- Practical4/triangle(2).py
+++ Practical4/triangle(2).py
@@ -1,5 +1,5 @@
 #There is no math skill in this version
- for i in range (1,11):
+for i in range (1,11):
  sum=0                     #defining the sum
  for j in range (1,i+1):   #adding the numbers
    sum += j                #calculating the sum
