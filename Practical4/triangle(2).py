c01222aafe6e89078ed3aa1ac2ec1e1f8ce6cd4e
Deloste <13586663666@139.com>
2025-03-30 23:39:33 +0800

    Create triangle(2).py

---
+++ Practical4/triangle(2).py
@@ -0,0 +1,6 @@
+#There is no math skill in this version
+ for i in range (1,11):
+ sum=0                     #defining the sum
+ for j in range (1,i+1):   #adding the numbers
+   sum += j                #calculating the sum
+ print('T', i, '=', sum)   #print the outcome
