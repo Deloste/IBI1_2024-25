30d4847394a22e4e0612d8fa93bba640eb4f186f
Deloste <13586663666@139.com>
2025-03-30 23:41:42 +0800

    Update and rename triangle.py to triangle(1).py

---
+++ Practical4/triangle(1).py
@@ -0,0 +1,5 @@
+#In this version I used a math skill so it is faster. There is another version without math skill in "triangle(2).py"
+for i in range(1,11): #start the calculation    
+    ti=0.5*(i+1)*i    #Ti=0.5*i*(i+1)
+    print('Ti=',ti)   #print the outcome 
+
